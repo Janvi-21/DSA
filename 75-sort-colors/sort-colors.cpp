0fd1c65abec4e7ebbceaa26034208a9b23bc32d8
Janvi chuphal <124714640+Janvi-21@users.noreply.github.com>
2026-09-21 11:26:44 +0530

    Time: 0 ms (100.00%) | Memory: 11.5 MB (98.03%) - LeetSync

---
+++ 75-sort-colors/sort-colors.cpp
@@ -0,0 +1,16 @@
+class Solution {
+public:
+    void sortColors(vector<int>& nums) {
+        int n = nums.size();
+        for(int i = 0; i < n-1; i++){
+            int mini = i;
+
+            for(int j = i ; j < n; j++){
+                if(nums[j] < nums[mini]) {mini = j;}
+            }
+
+            swap(nums[mini], nums[i]);
+        }
+        
+    }
+};
